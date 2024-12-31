7e6ea9460f34fa93325e684af46fba8226e14fe9
Fitsum <166730412+konjitfitsum@users.noreply.github.com>
2024-12-31 15:15:29 +0000

    initial commit  for evangadi forum deployment

---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbConnection = mysql2.createPool({
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  connectionLimit: process.env.CONNECTION_LIMIT,
+  port: process.env.PORT,
+});
+
+export default dbConnection.promise();
